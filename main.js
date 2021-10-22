0c6603b6e2ae8624be35e04a62709907338c77eb
Roberto Paz <fsjedi@outlook.com>
2021-10-22 11:54:16 -0400

    arreglos hechos

---
+++ main.js
@@ -0,0 +1,11 @@
+const bobsFollowers = ['Tono', 'Silviux', 'Piumpi', 'Rob']
+
+const tinasFollowers = ['Sam', 'Tono', 'Aldo', 'Piumpi']
+
+
+/*
+Make a variable called tinasFollowers and set it 
+equal to an array with three strings representing the 
+names of Tina’s friends. Make exactly two of these the same as 
+two of the friends in the bobsFollowers array.
+*/
